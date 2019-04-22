c1103a066377474f6c55dfa7ec2b631a03068aab
Abel Acosta <abelacosta1596@gmail.com>
2019-04-21 23:20:33 -0700

    Created ghosts

--- src/com/blu3flux/Game.java
+++ src/com/blu3flux/Game.java
@@ -6,17 +6,26 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 import com.blu3flux.entity.Food;
+import com.blu3flux.entity.Ghost;
 import com.blu3flux.entity.Pacman;
 
 public class Game {
 	
 	public Level level1;
 	public Pacman pacman;
+	public Ghost blinky;
+	public Ghost pinky;
+	public Ghost inky;
+	public Ghost clyde;
 	public ArrayList<Food> food = new ArrayList<Food>();
 	
 	public Game() {
 		level1 = new Level("assets/level1.png");
 		pacman = new Pacman("assets/pacman.png");
+		blinky = new Ghost(400, 322,"assets/blinky.png");
+		inky = new Ghost(357, 390,"assets/inky.png");
+		pinky = new Ghost(400, 390,"assets/pinky.png");
+		clyde = new Ghost(443, 390,"assets/clyde.png");
 		readLevelData();
 		
 	}
