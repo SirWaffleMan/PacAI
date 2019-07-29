e4af3db51a61cb8f288e5eb34e8b0e2efa9827a4
Abel Acosta <abelacosta1596@gmail.com>
2019-07-29 12:39:05 -0700

    Added WASD as additional controls

--- src/com/blu3flux/input/Input.java
+++ src/com/blu3flux/input/Input.java
@@ -17,15 +17,19 @@ public class Input implements KeyListener{
 	@Override
 	public void keyPressed(KeyEvent e) {
 		switch(e.getKeyCode()) {
+		case KeyEvent.VK_W:
 		case KeyEvent.VK_UP:
 			move.isMovingUp = true;
 			break;
+		case KeyEvent.VK_D:
 		case KeyEvent.VK_RIGHT:
 			move.isMovingRight = true;
 			break;
+		case KeyEvent.VK_S:
 		case KeyEvent.VK_DOWN:
 			move.isMovingDown = true;
 			break;
+		case KeyEvent.VK_A:
 		case KeyEvent.VK_LEFT:
 			move.isMovingLeft = true;
 			break;
@@ -35,15 +39,19 @@ public class Input implements KeyListener{
 	@Override
 	public void keyReleased(KeyEvent e) {
 		switch(e.getKeyCode()) {
+		case KeyEvent.VK_W:
 		case KeyEvent.VK_UP:
 			move.isMovingUp = false;
 			break;
+		case KeyEvent.VK_D:
 		case KeyEvent.VK_RIGHT:
 			move.isMovingRight = false;
 			break;
+		case KeyEvent.VK_S:
 		case KeyEvent.VK_DOWN:
 			move.isMovingDown = false;
 			break;
+		case KeyEvent.VK_A:
 		case KeyEvent.VK_LEFT:
 			move.isMovingLeft = false;
 			break;
